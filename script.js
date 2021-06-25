56eee48dd5e424bca35bec619b6ed311c246ee18
Renchester Ramos <renchesterjramos@gmail.com>
2021-06-25 23:36:12 +0800

    Finalize game logicy and refactor code

---
+++ script.js
@@ -0,0 +1,101 @@
+'use strict';
+
+// DOM Selection
+let btnStart = document.querySelector('.btn-start');
+
+// Gameplay
+const choices = ['rock', 'paper', 'scissors'];
+let scores = [0, 0];
+let roundsPlayed = 0;
+
+function capitalizeFirst(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function computerPlay() {
+  const choiceIndex = Math.ceil(Math.random() * choices.length) - 1;
+  const computerSelection = choices[choiceIndex];
+  return computerSelection;
+}
+// computerPlay();
+
+function gameReset() {
+  scores = [0, 0];
+  playerScore = 0;
+  computerScore = 0;
+  roundsPlayed = 0;
+}
+
+function playRound() {
+  let roundResult;
+
+  const playerInput = prompt('Pick between rock, paper, or scissors', '');
+  const playerSelection = playerInput.trim().toLowerCase();
+  const computerSelection = computerPlay();
+
+  if (playerSelection === computerSelection) {
+    roundResult = 'tie';
+  } else if (playerSelection === 'rock') {
+    roundResult =
+      computerSelection === 'scissors' ? 'playerWin' : 'computerWin';
+  } else if (playerSelection === 'scissors') {
+    roundResult = computerSelection === 'paper' ? 'playerWin' : 'computerWin';
+  } else if (playerSelection === 'paper') {
+    roundResult = computerSelection === 'rock' ? 'playerWin' : 'computerWin';
+  } else alert('Wrong input! Try again.');
+
+  return [roundResult, playerSelection, computerSelection];
+}
+
+function game() {
+  const winningScore = 5;
+  let playerScore = scores[0];
+  let computerScore = scores[1];
+  let resultStr;
+
+  // Initializing the game
+  if (roundsPlayed === 0) {
+    alert("Let's play rock, paper scissors!");
+  }
+
+  // Continuing gameplay if no one has won
+  if (playerScore < winningScore && computerScore < winningScore) {
+    const [roundResult, playerSelection, computerSelection] = playRound();
+
+    if (roundResult === 'playerWin') {
+      resultStr = `You win! ${capitalizeFirst(
+        playerSelection
+      )} beats ${computerSelection}`;
+      playerScore++;
+    } else if (roundResult === 'computerWin') {
+      resultStr = `You lose! ${capitalizeFirst(
+        computerSelection
+      )} beats ${playerSelection}.`;
+      computerScore++;
+    } else if (roundResult === 'tie') {
+      resultStr = `Tie! No one wins this round`;
+    } else if (!roundResult) game();
+
+    scores = [playerScore, computerScore];
+    roundsPlayed++;
+
+    alert(
+      `${resultStr} \n Your score: ${playerScore} \n Computer score: ${computerScore} \n Tries: ${roundsPlayed}`
+    );
+
+    game();
+  }
+
+  // Result if a player has won
+
+  if (playerScore === winningScore) {
+    alert(`You beat the computer in ${roundsPlayed} tries! Nice!`);
+    gameReset();
+  } else if (computerScore === winningScore) {
+    alert(`Damn, you lost even with ${roundsPlayed} tries. Tough luck!`);
+    gameReset();
+  }
+}
+
+// Event Listeners
+btnStart.addEventListener('click', game);
